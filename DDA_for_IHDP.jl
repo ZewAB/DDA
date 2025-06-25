7089101d21351016f8243dccc12c861950e93abf
ZewAB <80992109+ZewAB@users.noreply.github.com>
2025-06-25 21:40:53 +0300

    Update DDA_for_IHDP.jl

--- DDA_for_IHDP.jl
+++ DDA_for_IHDP.jl
@@ -99,14 +99,11 @@ function SCPRSM_bo1(α, λ, r, WX, YX, Y, X, balance_target, chi, tau)
   # Regularization weights
   λ₁ = λ * α
   λ₂ = λ * (1.0 - α)
-
-  # L1 block (γ)
   gradγ = zero(c)
   hγ = LeastSquares(WX, YX)          # Loss function for L1
   fγ = Translate(hγ, v)              # Translated loss
   gγ = NormL1(λ₁)                    # Regularization function for L1
 
-  # L0 block (δ)
   gradδ = zero(d)
   hδ = LeastSquares(WX, YX)          # Loss function for L0
   fδ = Translate(hδ, u)              # Translated loss
